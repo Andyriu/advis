c3c2f106926621910dd75fbd581b779cff107724
Andyriu <andyriu727@gmail.com>
2025-03-17 18:51:47 -0500

    Update api chechPlayer

--- src/app/api/checkPlayer/route.ts
+++ src/app/api/checkPlayer/route.ts
@@ -1,34 +1,43 @@
-'use server'
-
 import { createClient } from "@/utils/supabase/server"
+import { NextResponse } from "next/server"
 
-export async function POST (req: Request): Promise<Response> {
+interface IData {
+   id: string
+   name: string | null
+}
+
+interface IChecKPlayerResponse {
+   valid: boolean
+   data?: IData
+   error?: string
+}
+
+export async function POST (req: Request): Promise<NextResponse<IChecKPlayerResponse>> {
    const supabase = await createClient()
+   console.log(req)
    const {email} = await req.json()
-
+   console.log(email, "esta mostrando alfo?")
    const {data, error} = await supabase
       .from('players')
       .select('*')
       .eq('email', email)
    
    if (error) {
-      console.error ('Error en la base de datos', error)
-      return new Response(
-         JSON.stringify({ errror: 'Error mirando el correo' }),
+      return NextResponse.json(
+         { valid: false, errror: 'Error mirando el correo' },
          { status: 500 }
       )
    }
    
    const player = data[0];
-   console.log(player, "pruba y erorr", player.name)
    if (data.length === 0) {
-      return new Response(
-         JSON.stringify({ exists: false }),
+      return NextResponse.json(
+         { valid: false, error: "No esta registrado en player"},
          { status: 200 }
       )
    } else {
-      return new Response(
-         JSON.stringify({ exists: true, player: {id: player.player_id, name: player.name} }),
+      return NextResponse.json(
+         { valid: true, data: {id: player.player_id, name: player.name} },
          { status: 200 }
       )
    }
