03ebddb518154734563d85d799e1a24aec79fa4e
Andyriu <andyriu727@gmail.com>
2025-03-17 18:11:09 -0500

    change hook improvement useCheckEmail.ts

--- src/hooks/useCheckEmail.ts
+++ src/hooks/useCheckEmail.ts
@@ -1,40 +1,31 @@
 import { useState } from "react";
 
 interface ICheckEmailResponse {
-   exist: boolean
-   data?: object
+   exist: boolean;
+   data?: object;
+   error?:string
 }
 
 const useCheckEmail = () => {
-   const [loading, setLoading] = useState(false)
-   const [error, setError] = useState<string | null> (null)
-
-   const checkEmail = async (email:string): Promise<ICheckEmailResponse> => {
-      setLoading (true)
-      setError (null)
-
-      try {
-         const response = await fetch('/api/chekEmail',{
-            method: 'POST',
-            headers: {
-               "content-Type": "application/json"
-            },
-            body: JSON.stringify({email})
-         })
-         const { exists } = await response.json()
-         console.log(exists, "soy sumamente estupido")
-         if (!response.ok) {
-            throw new Error("Error al verificar el correo electrónico")
-         }
-         return { exist: exists }
-      } catch (err) {
-         setError(err.message)
-         return {exist: false}
-      } finally {
-         setLoading(false)
+   const checkEmail = async (email: string): Promise<ICheckEmailResponse> => {
+      const response = await fetch("/api/chekEmail", {
+         method: "POST",
+         headers: {
+            "content-Type": "application/json",
+         },
+         body: JSON.stringify({ email }),
+      });
+      const { exists, player, error} = await response.json();
+      if (error) {
+         return {exist: exists, error: error}
+      } else if (!exists) {
+         return {exist: exists, error: error}
+      } else {
+         return {exist: exists, data: player}
       }
-   }
-   return {checkEmail, loading, error  }
-}
+      
+   };
+   return { checkEmail };
+};
 
-export default useCheckEmail
+export default useCheckEmail;
