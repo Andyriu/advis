b0bc14cccda035c70abb97355b755db7a8238ffd
Andyriu <andyriu727@gmail.com>
2025-03-17 18:43:12 -0500

    Update hook improvement useLogin

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,25 +1,28 @@
-interface ILoginResponse{
-   valid: boolean
-   error?: string
+interface ILoginResponse {
+   valid: boolean;
+   error?: string;
 }
 
 const useLogin = () => {
-   const Login = async (email: string, password: string): Promise<ILoginResponse> => {
-      const response = await fetch('/api/Login', {
-         method: 'POST',
+   const login = async (
+      email: string,
+      password: string,
+   ): Promise<ILoginResponse> => {
+      const response = await fetch("/api/Login", {
+         method: "POST",
          headers: {
             "content-Type": "application/json",
          },
          body: JSON.stringify({ email, password }),
-      })
-      const {valid, error} = await response.json()
+      });
+      const { valid, error } = await response.json();
       if (!valid) {
-         return {valid: valid, error: error}
+         return { valid: valid, error: error };
       } else {
-         return {valid: valid}
+         return { valid: valid };
       }
-   }
-   return {Login}
-}
+   };
+   return { login };
+};
 
 export default useLogin;
