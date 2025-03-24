c1b979173722e3d95b8384c414ac6a74400a8266
Andyriu <andyriu727@gmail.com>
2025-03-24 12:18:58 -0500

    Update utils commands

--- src/utils/commands/commands.ts
+++ src/utils/commands/commands.ts
@@ -3,12 +3,20 @@ export const commands = {
       return "comandos disponibles: help, setlife, setdefense"
    },
 
-   setlife: (value, setLife) => {
-      if (!isNaN(value)) {
-         setLife(Number (value))
-         return 'vida establecida a ${value}'
+   setlife: (value: string, setHealth: (value: number) => void) => {
+      if (!isNaN(Number(value))) {
+         setHealth(Number (value))
+         return `vida establecida a ${value}`
       } else {
          return "eror: El valor de vida debe ser un numero"
+      }
+   },
+
+   buy: async (value: string, executeBuy: (object_id: number) => Promise<string>) => {
+      if (!isNaN(Number(value))) {
+         return await executeBuy(Number(value));
+      } else {
+         return "Error: El objeto debe ser un valor numérico";
       }
    }
 }
