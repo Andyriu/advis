ec62673bf54a1dc6bb31c57666682043301e682c
Andyriu <andyriu727@gmail.com>
2025-03-24 12:16:50 -0500

    Update page game

--- src/app/(game)/game/page.tsx
+++ src/app/(game)/game/page.tsx
@@ -7,54 +7,68 @@ import { createClient } from "@/utils/supabase/client";
 import styles from "./page.module.css";
 import { commands } from "@/utils/commands/commands";
 import usePlayer from "@/hooks/usePlayer";
+import useBuyCommand from "@/hooks/useBuyCOmmnand";
 
 export default function GamePage() {
    const router = useRouter();
    const inputRef = useRef(null);
 
-   const {player, uuid, name, health, defense, money, level, exp} = usePlayer();
+   const {player, uuid, name, health, defense, money, level, exp, updateAttributes} = usePlayer();
+   const { executeBuy } = useBuyCommand();
 
    const [loading, setLoading] = useState(true);
    const [comand, setComand] = useState<string>("");
+   const [validInitial, setValidInitial] = useState<boolean>(false)
 
    useEffect(() => {
-      const fetchData = async () => {
-         const supabase = createClient();
-         const { data, error } = await supabase.auth.getUser();
-         if (error || !data?.user) {
-            router.push("/login");
-         } else {
-            const {exist, error} = await player(data.user.email)
-            if (exist) {
-               setLoading(false)
+      if (!validInitial) {
+         const fetchData = async () => {
+            const supabase = createClient();
+            const { data, error } = await supabase.auth.getUser();
+            if (error || !data?.user) {
+               router.push("/login");
             } else {
-               console.log(error)
-               router.push('/game/initial')
+               const {exist, error} = await player(data.user.email)
+               if (exist) {
+                  setValidInitial(true)
+                  setLoading(false)
+               } else {
+                  console.log(error)
+                  router.push('/game/initial')
+               }
             }
+         };
+         fetchData();
+      }
+   }, [validInitial, router, player]);
+
+
+   const handleCommand = useCallback (
+      (commandText) => {
+         const [command, ...args] = commandText.split(" ");
+         const action = commands[command];
+   
+         if (action) {
+            if (command === "buy") {
+               return action(args[0], executeBuy);
+            } else {
+               return action(...args);
+            }
+         } else {
+            return `Comando no reconocido: ${command}`;
          }
-      };
-      fetchData();
-   }, [router, player]);
+      }, [executeBuy]
+   )
 
    const executeCommand = useCallback(
       (commandText) => {
          const result = handleCommand(commandText);
+         updateAttributes(uuid)
          console.log(result);
          setComand("");
       },
-      [],
+      [handleCommand, updateAttributes, uuid],
    );
-
-   const handleCommand = (commandText) => {
-      const [command, ...args] = commandText.split(" ");
-      const action = commands[command];
-
-      if (action) {
-         return action(...args,);
-      } else {
-         return `Comando no reconocido: ${command}`;
-      }
-   };
 
    useEffect(() => {
       const inputElement = inputRef.current;
@@ -131,13 +145,13 @@ export default function GamePage() {
                </div>
                <div className="p-7 bg-unique-500 rounded-xl">
                   <h1 className="bg-unique-100">como esta distribuido esto?</h1>
-                  {name && <p className="bg-unique-100">nombre:{name}</p>}
-                  {name && <p className="bg-unique-100">id:{uuid}</p>}
-                  {name && <p className="bg-unique-100">vida:{health}</p>}
-                  {name && <p className="bg-unique-100">defensa:{defense}</p>}
-                  {name && <p className="bg-unique-100">Nivel:{level}</p>}
-                  {name && <p className="bg-unique-100">Exp:{exp}</p>}
-                  {name && <p className="bg-unique-100">Dinero:{money}</p>}
+                  {validInitial && <p className="bg-unique-100">nombre:{name}</p>}
+                  {validInitial && <p className="bg-unique-100">id:{uuid}</p>}
+                  {validInitial && <p className="bg-unique-100">vida:{health}</p>}
+                  {validInitial && <p className="bg-unique-100">defensa:{defense}</p>}
+                  {validInitial && <p className="bg-unique-100">Nivel:{level}</p>}
+                  {validInitial && <p className="bg-unique-100">Exp:{exp}</p>}
+                  {validInitial && <p className="bg-unique-100">Dinero:{money}</p>}
                </div>
             </div>
             <div>
