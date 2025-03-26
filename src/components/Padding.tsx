8f2361451fc22f61fc565df04b41610925930676
Andyriu <andyriu727@gmail.com>
2025-03-26 14:50:12 -0500

    Remove compooent

--- src/components/Padding.tsx
+++
@@ -1,7 +0,0 @@
-export default function Padding ({children,}: Readonly<{children: React.ReactNode;}>) {
-   return (
-      <div style={{ paddingLeft: '100px', paddingRight: '100px' }}>
-         {children}
-      </div>
-   )
-}
